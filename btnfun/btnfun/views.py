e24d2ab3c114933dd2a717b6866a5597007ac553
Darcy Liu <darcy.ldx@gmail.com>
2023-12-23 11:38:02 -0500

    Increment button, stored in database model.

--- btnfun/btnfun/views.py
+++ btnfun/btnfun/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 
 def increment(request):
     ''''''
-    number = request.session.get('number', 0)
+    number = request.session.get('number')
     number += 1
     request.session['number'] = number
     return JsonResponse({'number': number})
@@ -11,4 +11,5 @@ def increment(request):
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    number = request.session.get('number')
+    return render(request, 'index.html', {'number': number})
